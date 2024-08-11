49da3041fc987e6c567c5b3935c7580157e1ac0b
Rohit Bharmal <rbharmal@redhat.com>
2024-08-11 17:25:04 +0530

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainPage from './components/MainPage.vue'
+</script>
+
+<template>
+  <main>
+    <MainPage></MainPage>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="">
+      <div class="input-container">
+        <label for="input-box">Search here: </label>
+        <pf-text-input id="input-box" type="text" placeholder="Add your name here"></pf-text-input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import '@patternfly/elements/pf-text-input/pf-text-input.js';
+</script>
+
+<style scoped>
+.container {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+
+.input-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
